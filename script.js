24a0a9574d4db032322ddb713cad0ae9dae7badf
SamuelCallWind <Samcallwind@gmail.com>
2023-06-06 14:03:55 +0200

    Set the start button to launch the countdown

---
+++ script.js
@@ -0,0 +1,40 @@
+let addMinute = document.querySelector('.buttonPlus');
+let deduceMinute = document.querySelector('.buttonMinus');
+let timerScreen = document.querySelector('.timer');
+let resetButton = document.querySelector('.reset');
+let startButton = document.querySelector('.buttonOne');
+let pauseButton = document.querySelector('.buttonTwo');
+
+function timeChange(numberOfMinutes) {
+    let timerContent = timerScreen.innerHTML.split(':').map(content => Number(content));
+    timerContent[0] += numberOfMinutes;
+    if (timerContent[0] > 60){
+        timerContent = 60;
+    } 
+    else if (timerContent[0] <= 0) {
+        timerContent = 0;
+    }
+    timerScreen.innerHTML = timerContent.map(part => part.toString().padStart(2, '0')).join(':');
+    }
+
+    function countdown(){
+        let timerContent = timerScreen.innerHTML.split(':').map(content => Number(content));
+        let timerSeconds = timerContent[0] * 60 + timerContent[1];
+        countInterval = setInterval(() => {
+            if (timerContent[0] === 0 && timerContent[1] === 0) {
+                clearInterval(countdownInterval);
+            } else if (timerContent[1] === 0 && timerContent[0] >= 1) {
+                timerContent[0] -= 1;
+                timerContent[1] = 59;
+            } else {
+                timerContent[1] -= 1;
+            }
+            timerScreen.innerHTML = timerContent.map(part => part.toString().padStart(2, '0')).join(':');
+
+        }, 1000);
+    }
+
+addMinute.addEventListener('click', () => timeChange(1));
+deduceMinute.addEventListener('click', () => timeChange(-1));
+resetButton.addEventListener('click', () => timerScreen.innerHTML = "25:00");
+startButton.addEventListener('click', () => countdown())
